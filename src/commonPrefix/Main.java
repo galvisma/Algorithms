b7371fceb3be9d1e62a5d38e765ff70f0ee87fc7
galvisma <galvisma2017@gmail.com>
2023-03-29 19:05:25 -0500

    Last version commonPrefix

--- src/commonPrefix/Main.java
+++ src/commonPrefix/Main.java
@@ -3,9 +3,10 @@ package commonPrefix;
 public class Main {
     public static void main(String[] args) {
 
-        String[] strs = {"flower", "flight", "flights"};
+        // String[] strs = {"flower", "flight", "flights"};
+        // String[] strs = {"wanted","want", "wan"};
         // String[] strs = {"dog", "racecar", "car"};
-        // String[] strs = {"house","housekeeper","housewife", "car"};
+        String[] strs = {"house","housekeeper","housewife"};
 
         Solution longPrefix = new Solution();
         String commonPrefix = longPrefix.longestCommonPrefix(strs);
